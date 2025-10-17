4186e407f4c388324fbb748b1e168c0124bb007d
adialfatih <adisubuhadir@gmail.com>
2025-10-17 13:32:30 +0700

    update server app.js

--- app.js
+++ app.js
@@ -314,6 +314,10 @@ app.delete('/api/backups', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3003;
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+const HOST = process.env.HOST || '0.0.0.0';
+// server.listen(PORT, () => {
+//     console.log(`Server running at http://localhost:${PORT}`);
+// });
+server.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
 });
